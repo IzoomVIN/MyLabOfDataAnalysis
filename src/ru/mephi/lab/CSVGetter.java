3e681df21d17146f4c532ab6382f636015340cff
Vyacheslav Vin <Slava147148@gmail.com>
2019-03-16 11:24:15 +0300

    Class CSVReader is completed

--- src/ru/mephi/lab/CSVGetter.java
+++ src/ru/mephi/lab/CSVGetter.java
@@ -1,7 +1,6 @@
 package ru.mephi.lab;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,22 +12,57 @@ public class CSVGetter {
     public List<SuicideStatisticsRow> getListData(String docName){
         String path = String.format("../../../../files/%s.csv", docName);
         List<SuicideStatisticsRow> resultList = new ArrayList<>();
+        BufferedReader reader = null;
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+        try{
+            reader = new BufferedReader(new FileReader(path));
             String line = "";
             String csvSplitBy = ",";
 
             while ((line = reader.readLine()) != null){
-                String rowFromFile[] = line.split(csvSplitBy);
+                String[] rowFromFile = line.split(csvSplitBy);
                 SuicideStatisticsRow row = new SuicideStatisticsRow(rowFromFile[0], Integer.valueOf(rowFromFile[1]),
                         rowFromFile[2], rowFromFile[3], Integer.valueOf(rowFromFile[4]),
                         Integer.valueOf(rowFromFile[5]), Integer.valueOf(rowFromFile[6]));
 
-                /**Check on replicate*/
-
+                /** Check to reality and null suicide count*/
+                if (nullSuicideCountCheck(row) && checkToReality(row)) {
+                    /** Check to replicate in result list*/
+                    boolean FLAG = false;
+                    for (SuicideStatisticsRow rowFromArray : resultList) {
+                        if (row.equals(rowFromArray)) {
+                            FLAG = true;
+                            break;
+                        }
+                    }
+                    if (!FLAG) {
+                        resultList.add(row);
+                    }
+                }
             }
         } catch (IOException e){
             e.printStackTrace();
+        } finally {
+            if (reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
         }
+
+        return resultList;
+    }
+
+    private boolean nullSuicideCountCheck(SuicideStatisticsRow row){
+        return !(((row.getAge() == null ) || (row.getAge().equals(""))) ||
+                ((row.getCountry() == null ) || (row.getCountry().equals(""))) ||
+                ((row.getSex() == null ) || (row.getSex().equals(""))));
+    }
+
+    private boolean checkToReality(SuicideStatisticsRow row){
+        return ((row.getSuicidesCount() != row.getPopulation()) &&
+                ((row.getPopulation() - row.getSuicidesCount()) < (int)row.getPopulation()*0.7));
     }
 }
